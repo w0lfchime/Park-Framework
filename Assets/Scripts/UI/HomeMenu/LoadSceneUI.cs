51b07a7eb6a80ef016ff0759d8f3e496ae950b73
Jay Edwards <niter.park@gmail.com>
2025-05-14 13:22:16 -0400

    dev select and scene loader for home menu

---
+++ Assets/Scripts/UI/HomeMenu/LoadSceneUI.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneUI : MonoBehaviour
+{
+	[Header("Setup")]
+	public Transform contentParent;         // Scroll View's Content transform
+	public GameObject buttonPrefab;         // Button prefab reference
+	public string[] sceneNames;             // List of scenes to show
+
+	void Start()
+	{
+		foreach (var sceneName in sceneNames)
+		{
+			var btn = Instantiate(buttonPrefab, contentParent);
+			btn.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = sceneName;
+
+			btn.GetComponent<Button>().onClick.AddListener(() =>
+			{
+				SceneManager.LoadScene(sceneName);
+			});
+		}
+	}
+}
+
